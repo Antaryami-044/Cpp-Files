582f6bb46fae93e52139ef132c5f886ea7af9c99
Antaryami-044 <antaryamibabu1234@gmail.com>
2025-10-19 13:20:58 +0530

    5 questions

--- DSA_c++/D4.cpp
+++ DSA_c++/D4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int findLargest(vector<int> &arr)
@@ -53,14 +54,53 @@ int findSecondSmallest(vector<int> &arr)
     return ssmallest;
 }
 
+void check(vector<int> &arr)
+{   
+    cout << "Check function called!" << endl;
 
+    for(int i =0; i<arr.size() - 1; i++){
+        if(arr[i] <= arr[i+1]){
+            continue;
+        }
+        else{
+            cout << "Array is not sorted in non-decreasing order." << endl;
+            return;
+        }
+    }
+}
+
+vector<int> removeDuplicates(vector<int> &arr)
+{
+    sort(arr.begin(), arr.end());
+    vector<int> temp;
+    for(int i =0; i < arr.size()-1; i++) {
+        if(arr[i] != arr[i+1]) {
+            temp.push_back(arr[i]);
+        }
+    }
+    return temp;
+}
 
 int main()
 {
-    vector<int> arr = {1, 3, 5, 7, 9};
+    vector<int> arr = {1, 3, 8, 7, 9, 2, 5, 6, 4, 3, 8, 1};
+
     cout << "The largest element is: " << findLargest(arr) << endl;
+
     cout << "Hello" << endl;
+
     cout << "The second largest element is: " << findSecondLargest(arr) << endl;
+
     cout << "The second smallest element is: " << findSecondSmallest(arr) << endl;
+    
+    check(arr);
+
+    vector<int> uniqueArr = removeDuplicates(arr);
+
+    cout << "Array after removing duplicates: ";
+    
+    for(int i = 0; i < uniqueArr.size(); i++) {
+        cout << uniqueArr[i] << " ";
+    }
     return 0;
 }
