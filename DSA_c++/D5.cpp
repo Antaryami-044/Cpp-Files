f643abf9fad47e3ba1a0293585ae7e6387a75885
Antaryami-044 <antaryamibabu1234@gmail.com>
2025-10-21 00:15:56 +0530

    Day 5 on going

---
+++ DSA_c++/D5.cpp
@@ -0,0 +1,118 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> moveZeros(vector<int> &arr, int n) {
+    int count = 0;
+    for(int i=0; i <n; i++){
+        if(arr[i] != 0){
+            arr[count++] = arr[i];
+        }
+    }
+    for(int i = count; i < n; i++) {
+        arr[i] = 0;
+    }
+    return arr;
+}
+
+int linearSearch(vector<int> &arr, int n, int key) {
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+vector<int> mergeArrays(vector<int>& a, vector<int>& b) {
+    int i = 0, j = 0;
+    vector<int> merged;
+    while(i < a.size() && j < b.size()) {
+        int val;
+        if(a[i] < b[j]){
+            val = a[i++];
+        } else if(a[i] > b[j]){
+            val = b[j++];
+        } else{
+            val = a[i++];
+            j++;
+        }
+
+        if(merged.empty() || merged.back() != val) {
+            merged.push_back(val);
+        }
+    }
+
+    while(i < a.size()){
+        if(merged.empty() || merged.back() != a[i]) {
+            merged.push_back(a[i]);
+        }
+        i++;
+    }
+
+    while(j < b.size()){
+        if(merged.empty() || merged.back() != b[j]) {
+            merged.push_back(b[j]);
+        }
+        j++;
+    }
+    
+    return merged;
+}
+
+
+
+int main(){
+    // int n;
+    // cout << "Enter the size of the array: ";    
+    // cin >> n;
+    // vector<int> arr(n);
+    // cout << "Enter the elements of the array: ";
+    // for(int i = 0; i < n; i++) {
+    //     cin >> arr[i];
+    // }
+    // vector<int> result = moveZeros(arr, n);
+    // cout << "Array after moving zeros to the end: ";
+    // for(int i = 0; i < n; i++) {
+    //     cout << result[i] << " ";
+    // }
+
+    // int key;
+    // cout << "\nEnter the key to search: ";      
+    // cin >> key;
+    // int index = linearSearch(arr, n, key);  
+    // if(index != -1) {
+    //     cout << "Element found at index: " << index << endl;
+    // } else {
+    //     cout << "Element not found in the array." << endl;
+    // }
+
+
+    int m;
+    cout << "Enter the size of the second array: "; 
+    cin >> m;
+    vector<int> b(m);
+    cout << "Enter the elements of the second array: ";
+    for(int i = 0; i < m; i++) {
+        cin >> b[i];
+    }
+
+    int m1;
+    cout << "Enter the size of the third array: "; 
+    cin >> m1;
+    vector<int> c(m1);
+    cout << "Enter the elements of the 3rd array: ";
+    for(int i = 0; i < m1; i++) {
+        cin >> c[i];
+    }
+
+    vector<int> mergedArray = mergeArrays(b, c);  
+    cout << "Merged array: ";
+    for(int i = 0; i < mergedArray.size(); i++) {
+        cout << mergedArray[i] << " ";
+    }
+
+
+
+    return 0;
+}
