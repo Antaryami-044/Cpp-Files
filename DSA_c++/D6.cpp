0a6ed78ace36993db7b0a6bbc1dff383b767e72b
Antaryami-044 <antaryamibabu1234@gmail.com>
2025-10-21 19:06:57 +0530

    task-1

--- DSA_c++/D6.cpp
+++ DSA_c++/D6.cpp
@@ -2,6 +2,21 @@
 #include<vector>
 using namespace std;
 
+void findMissingNumber(vector<int> &arr, int n){
+    int sum = 0;
+    for(auto it : arr){
+        sum += it;
+    }
+
+    int sum2 = n * (n+1) / 2;
+    int missNumber =sum2 - sum;
+    cout << "Missing Number is: " << missNumber << endl;
+}
+
 int main(){
+    vector<int> arr = {1, 2, 4, 5, 6};
+    int n = 6; // Since numbers are from 1 to 6
+    findMissingNumber(arr, n);
+    
     return 0;
 }
