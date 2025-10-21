a2d037baa4a5ed61d1e4877f57879ecd6527204f
Antaryami-044 <antaryamibabu1234@gmail.com>
2025-10-21 23:51:28 +0530

    2nd method updates

--- DSA_c++/D6.cpp
+++ DSA_c++/D6.cpp
@@ -13,10 +13,22 @@ void findMissingNumber(vector<int> &arr, int n){
     cout << "Missing Number is: " << missNumber << endl;
 }
 
+void usingXOR(vector<int> &arr, int n) {
+    int xor1 = 0, xor2 = 0;
+    for(int i=0; i<arr.size(); i++){
+        xor1 = xor1 ^ arr[i];
+        xor2 = xor2 ^ i+1;
+    }
+    xor2 = xor2 ^ n; // since numbers are from 1 to n
+    int missingNumber = xor1 ^ xor2;
+    cout << "Missing Number using XOR is: " << missingNumber << endl;
+}
+
 int main(){
     vector<int> arr = {1, 2, 4, 5, 6};
     int n = 6; // Since numbers are from 1 to 6
     findMissingNumber(arr, n);
+    usingXOR(arr, n);
     
     return 0;
 }
