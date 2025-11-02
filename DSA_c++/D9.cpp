d5f4041aca92fddeb21287a1d74a923f780a4218
Antaryami-044 <antaryamibabu1234@gmail.com>
2025-11-02 15:44:23 +0530

    partially day 9 complete

--- DSA_c++/D9.cpp
+++ DSA_c++/D9.cpp
@@ -89,22 +89,51 @@ void longestElementBF(vector<int> &arr, int n)
       cout << ls << endl;
 }
 
+void longestElementB(vector<int> &arr, int n){
+      int ls = 0;
+      int num, count = 0;
+      unordered_set<int> st;
+
+      for(int i=0; i<n; i++){
+            st.insert(arr[i]);
+      }
+
+      for(int i=0; i<n; i++){
+            num = arr[i];
+            count = 1;
+            if(st.find(num - 1) == st.end()){
+                  count = 1;
+                  num = arr[i];
+                  while(st.find(num + 1) != st.end()){
+                        count++;
+                        num++;
+                  }
+            }
+            ls = max(ls, count);
+      }
+      cout << ls << endl;
+}
+
 int main()
 {
 
       vector<int> arr = {2, 1, 5, 4, 3, 0, 0};
       vector<int> arr2 = {10, 22, 12, 3, 0, 6};
       vector<int> arr3 = {100, 4, 200, 1, 3, 2};
+      vector<int> arr4 = {5,8,4,3,2,1};
 
       int n = arr.size();
       int o = arr2.size();
       int p = arr3.size();
+      int q = arr4.size();
 
       nextPermutation(arr, n);
       cout << endl;
       superiorElement(arr2, o);
       cout << endl;
       longestElementBF(arr3, p);
+
+      longestElementB(arr4, q);
 
       return 0;
 }
