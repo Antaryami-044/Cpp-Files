1e300c1a99a0d2192d61451c72bcc65040bffab9
Antaryami-044 <antaryamibabu1234@gmail.com>
2025-11-03 23:22:10 +0530

    rotate matrix 90 degree problem sloved

--- DSA_c++/D9.cpp
+++ DSA_c++/D9.cpp
@@ -156,6 +156,28 @@ void setMatrixZero(vector<vector<int>> &matrix, int n, int m) {
       }
 }
 
+void rotateMatirx(vector<vector<int>> &matrix, int n, int m) {
+      for(int i=0; i<n-1; i++) {
+            for(int j=i; j<m; j++) {
+                  if(i != j) {
+                        swap(matrix[i][j], matrix[j][i]);
+                  }
+            }
+      }
+
+      for(int i=0; i<n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+      }
+
+
+      for(auto it : matrix){
+            for(auto newit : it){
+                  cout << newit  << "  ";
+            }
+            cout << endl;
+      }
+}
+
 int main()
 {
 
@@ -164,6 +186,7 @@ int main()
       vector<int> arr3 = {100, 4, 200, 1, 3, 2};
       vector<int> arr4 = {5,8,4,3,2,1};
       vector<vector<int>> matrix = {{1,0,0,1},{1,0,1,1},{1,1,0,1},{0,1,1,1}};
+      vector<vector<int>> matrix2 = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
 
       int n = arr.size();
       int o = arr2.size();
@@ -172,12 +195,13 @@ int main()
       int a = matrix.size();
       int b = matrix[0].size();
 
-      nextPermutation(arr, n);
-      cout << endl;
-      superiorElement(arr2, o);
-      cout << endl;
-      longestElementBF(arr3, p);
-      longestElementB(arr4, q);
-      setMatrixZero(matrix, a, b);
+      // nextPermutation(arr, n);
+      // cout << endl;
+      // superiorElement(arr2, o);
+      // cout << endl;
+      // longestElementBF(arr3, p);
+      // longestElementB(arr4, q);
+      // setMatrixZero(matrix, a, b);
+      rotateMatirx(matrix2, a, b);              // i use different matrix with smae size , so i donot assign the any size for matrix 2
       return 0;
 }
