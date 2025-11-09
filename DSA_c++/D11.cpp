6a816c2cc8e4e572e5aa367a635d04ac796f0ce8
Antaryami-044 <antaryamibabu1234@gmail.com>
2025-11-09 23:53:12 +0530

    brute force approch of 4 sum problem is complete

--- DSA_c++/D11.cpp
+++ DSA_c++/D11.cpp
@@ -1,3 +1,52 @@
-#include<iostream>
-#include<vector>
-#include<set>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+void fourSum_brute(vector<int> &arr, int n, int target)
+{
+      set<vector<int>> st;
+      for (int i = 0; i < n; i++)
+      {
+            for (int j = i + 1; j < n; j++)
+            {
+                  for (int k = j + 1; k < n; k++)
+                  {
+                        for (int l = k + 1; l < n; l++)
+                        {
+                              long long sum = arr[i] + arr[j] + arr[k] + arr[l];
+                              if (sum == target)
+                              {
+                                    vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
+                                    sort(temp.begin(), temp.end());
+                                    st.insert(temp);
+                              }
+                        }
+                  }
+            }
+      }
+
+      for (auto i : st)
+      {
+            for (auto j : i)
+            {
+                  cout << j << " ";
+            }
+            cout << endl;
+      }
+}
+
+int main()
+{
+
+      vector<int> arr1 = {1, 1, -1, 0, 2, -2, 3};
+
+      int n = arr1.size();
+
+      fourSum_brute(arr1, n, 4);
+
+      return 0;
+}
