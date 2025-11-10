162ae63c6a14ed168e000e3183177122319a9a31
Antaryami-044 <antaryamibabu1234@gmail.com>
2025-11-10 23:01:32 +0530

    find subarray using XOR is completed

--- DSA_c++/D11.cpp
+++ DSA_c++/D11.cpp
@@ -3,6 +3,7 @@
 #include <set>
 #include <algorithm>
 #include <math.h>
+#include <map>
 
 using namespace std;
 
@@ -39,76 +40,121 @@ void fourSum_brute(vector<int> &arr, int n, int target)
       }
 }
 
-void fourSum_better(vector<int> &arr, int n, int target) {
+void fourSum_better(vector<int> &arr, int n, int target)
+{
       set<vector<int>> st;
-      for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
+      for (int i = 0; i < n; i++)
+      {
+            for (int j = i + 1; j < n; j++)
+            {
                   set<int> hashmap;
-                  for(int k=j+1; k<n; k++) {
-                        int fourth =  target - (arr[i] + arr[j] + arr[k]);
-                        if(hashmap.find(fourth) != hashmap.end()) {
+                  for (int k = j + 1; k < n; k++)
+                  {
+                        int fourth = target - (arr[i] + arr[j] + arr[k]);
+                        if (hashmap.find(fourth) != hashmap.end())
+                        {
                               vector<int> temp = {arr[i], arr[j], arr[k], fourth};
                               sort(temp.begin(), temp.end());
                               st.insert(temp);
                         }
                         hashmap.insert(arr[k]);
-                  }     
+                  }
             }
       }
-      for(auto it : st) {
-            for(auto ele: it) {
-                  cout<< ele << " ";
+      for (auto it : st)
+      {
+            for (auto ele : it)
+            {
+                  cout << ele << " ";
             }
-            cout<< endl;
-      } 
+            cout << endl;
+      }
 }
 
-void fourSum_optimal(vector<int> &arr, int n, int target) {
+void fourSum_optimal(vector<int> &arr, int n, int target)
+{
       vector<vector<int>> ans;
       sort(arr.begin(), arr.end());
-      for(int i=0; i<n; i++) {
-            if(i>0 && arr[i]== arr[i-1]) continue;
-            for(int j=i+1; j<n; j++) {
-                  if(j>i+1 && arr[j] == arr[j-1]) continue;
-                  int k = j+1;
-                  int l = n-1;
-                  while(k<l) {
+      for (int i = 0; i < n; i++)
+      {
+            if (i > 0 && arr[i] == arr[i - 1])
+                  continue;
+            for (int j = i + 1; j < n; j++)
+            {
+                  if (j > i + 1 && arr[j] == arr[j - 1])
+                        continue;
+                  int k = j + 1;
+                  int l = n - 1;
+                  while (k < l)
+                  {
                         long long sum = arr[i] + arr[j] + arr[k] + arr[l];
-                        if(sum == target) {
+                        if (sum == target)
+                        {
                               vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
                               ans.push_back(temp);
                               k++;
                               l--;
-                              while(k<l && arr[k] == arr[k-1]) k++;
-                              while(k<l && arr[l] == arr[l+1]) l++;
-                        } else if(sum < target) {
+                              while (k < l && arr[k] == arr[k - 1])
+                                    k++;
+                              while (k < l && arr[l] == arr[l + 1])
+                                    l++;
+                        }
+                        else if (sum < target)
+                        {
                               k++;
-                        } else {
+                        }
+                        else
+                        {
                               l--;
                         }
                   }
             }
-
       }
-      for(auto it : ans) {
-            for(auto ele: it) {
-                  cout<< ele << " ";
+      for (auto it : ans)
+      {
+            for (auto ele : it)
+            {
+                  cout << ele << " ";
             }
-            cout<< endl;
+            cout << endl;
       }
+}
+
+void findSubArrayByXOR(vector<int> &arr, int n, int k)
+{
+      map<int, int> prefix;
+      int count = 0;
+      int XOR = 0;
+      for (int i = 0; i < n; i++)
+      {
+            XOR = XOR ^ arr[i];
+            if (XOR == count)
+            {
+                  count++;
+            }
+            int rem = XOR ^ k;
+            if (prefix.find(rem) != prefix.end())
+            {
+                  count += prefix[rem];
+            }
+            prefix[XOR]++;
+      }
+      cout << count << endl;
 }
 
 int main()
 {
 
       vector<int> arr1 = {1, 1, -1, 0, 2, -2, 3};
+      vector<int> arr2 = {2, 2, 2, 2, 2};
+      vector<int> arr3 = {1, 0, -1, 0, -2, 2};
 
       int n = arr1.size();
 
       // fourSum_brute(arr1, n, 4);
       // fourSum_better(arr1, n, 4);
-      fourSum_optimal(arr1, n, 4);
-
+      // fourSum_optimal(arr1, n, 4);
+      findSubArrayByXOR(arr1, n, 0);
 
       return 0;
 }
