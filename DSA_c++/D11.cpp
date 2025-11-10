c7fb1922661986ae7094c04b29e7ba1cebfc99a8
Antaryami-044 <antaryamibabu1234@gmail.com>
2025-11-10 20:50:26 +0530

    4 sum problem completed

--- DSA_c++/D11.cpp
+++ DSA_c++/D11.cpp
@@ -63,8 +63,39 @@ void fourSum_better(vector<int> &arr, int n, int target) {
       } 
 }
 
-void fourSum_optimal() {
-      
+void fourSum_optimal(vector<int> &arr, int n, int target) {
+      vector<vector<int>> ans;
+      sort(arr.begin(), arr.end());
+      for(int i=0; i<n; i++) {
+            if(i>0 && arr[i]== arr[i-1]) continue;
+            for(int j=i+1; j<n; j++) {
+                  if(j>i+1 && arr[j] == arr[j-1]) continue;
+                  int k = j+1;
+                  int l = n-1;
+                  while(k<l) {
+                        long long sum = arr[i] + arr[j] + arr[k] + arr[l];
+                        if(sum == target) {
+                              vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
+                              ans.push_back(temp);
+                              k++;
+                              l--;
+                              while(k<l && arr[k] == arr[k-1]) k++;
+                              while(k<l && arr[l] == arr[l+1]) l++;
+                        } else if(sum < target) {
+                              k++;
+                        } else {
+                              l--;
+                        }
+                  }
+            }
+
+      }
+      for(auto it : ans) {
+            for(auto ele: it) {
+                  cout<< ele << " ";
+            }
+            cout<< endl;
+      }
 }
 
 int main()
@@ -75,7 +106,9 @@ int main()
       int n = arr1.size();
 
       // fourSum_brute(arr1, n, 4);
-      fourSum_better(arr1, n, 4);
+      // fourSum_better(arr1, n, 4);
+      fourSum_optimal(arr1, n, 4);
+
 
       return 0;
 }
