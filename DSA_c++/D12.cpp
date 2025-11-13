708a1a44c5a527910d137211d191a02f7fc0a023
Antaryami-044 <antaryamibabu1234@gmail.com>
2025-11-13 12:47:35 +0530

    4 approaches added successfully

--- DSA_c++/D12.cpp
+++ DSA_c++/D12.cpp
@@ -55,14 +55,71 @@ void FMN2 (vector<int> & arr, int n) {
       cout << "Repeating Number is: " << int(y) << endl;
 }
 
+void FNM3(vector<int> & arr, int n) {
+      int xr = 0;
+      int bit = 0;
+      for(int i=0; i<n; i++) {
+            xr ^= arr[i];
+            xr ^= (i+1);
+      }
+      while(true) {
+            if( (xr & (1 << bit)) != 0) {
+                  break;
+            }
+            bit++;
+      }
+      
+      int x=0, y=0;
+
+      for(int i=0; i<n; i++) {
+            if( (arr[i] & (1 << bit)) != 0) {
+                  // group 1
+                  x ^= arr[i];
+            } else {
+                  // group 2
+                  y ^= arr[i];
+            }
+      }
+
+      for(int i=1; i<=n; i++) {
+            if( (i & (1 << bit)) != 0) {
+                  // group 1
+                  x ^= i;
+            } else {
+                  // group 2
+                  y ^= i;
+            }
+      }
+
+      int cnt = 0;
+      int missingNumber = 0, repeatingNumber = 0;
+      for(int i=0; i<n; i++) {
+            if(arr[i] == y) {
+                  cnt++;
+            }
+      }
+      if(cnt == 2) {
+                  missingNumber = x;
+                  repeatingNumber = y;
+      } else {
+                  missingNumber = y;
+                  repeatingNumber = x;
+      }
+
+      cout << "Missing Number is: " << missingNumber << endl;
+      cout << "Repeating Number is: " << repeatingNumber << endl;
+
+}
+
 int main() {
 
-      vector<int> arr = {6, 1, 5, 4, 6, 2};
+      vector<int> arr = {1, 1, 3, 4, 6, 2};
 
       int n = arr.size();
 
-      find_missing_Number(arr, n);
-      FMN2(arr, n);
+      // find_missing_Number(arr, n);
+      // FMN2(arr, n);
+      FNM3(arr, n);
 
       return 0;
 }
