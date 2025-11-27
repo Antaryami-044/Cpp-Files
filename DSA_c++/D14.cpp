d561ce143dde2f5979aae7917bdcd0401c89ffc2
Antaryami-044 <antaryamibabu1234@gmail.com>
2025-11-27 17:20:57 +0530

    part of Binary Search problem solved

--- DSA_c++/D14.cpp
+++ DSA_c++/D14.cpp
@@ -19,11 +19,30 @@ int lowerBound(vector<int> &arr, int target) {
       return ans;
 }
 
+int upperBound(vector<int> &arr, int target) {
+      int low = 0, high = arr.size() - 1;
+      int ans = arr.size();
+      while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(arr[mid] > target) {
+                  ans = mid;
+                  high = mid - 1;
+            } else {
+                  low = mid + 1;
+            }
+      }
+      return ans;
+}
+
 int main() {
       vector<int> arr = {1, 3, 3, 5, 7, 9};
       int target = 3;
+
       int index = lowerBound(arr, target);
-      cout << "The lower bound index of " << target << " is: " << index << endl;    
+      int index2 = upperBound(arr, target);
+
+      cout << "The lower bound index of " << target << " is: " << index << endl; 
+      cout << "The upper bound index of " << target << " is: " << index2 << endl;   
 
       return 0;
 }
