5e915f2b0337dd10df6c229c217fce5d4de626e3
Antaryami-044 <antaryamibabu1234@gmail.com>
2025-11-27 16:54:04 +0530

    Day 14 started

---
+++ DSA_c++/D14.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<math.h>
+
+using namespace std;
+
+int lowerBound(vector<int> &arr, int target) {
+      int low = 0, high = arr.size() - 1;
+      int ans = arr.size();
+      while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(arr[mid] >= target) {
+                  ans = mid;
+                  high = mid - 1;
+            } else {
+                  low = mid + 1;
+            }
+      }
+      return ans;
+}
+
+int main() {
+      vector<int> arr = {1, 3, 3, 5, 7, 9};
+      int target = 3;
+      int index = lowerBound(arr, target);
+      cout << "The lower bound index of " << target << " is: " << index << endl;    
+
+      return 0;
+}
