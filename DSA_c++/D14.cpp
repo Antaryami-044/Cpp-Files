66805a00aa0f7b0a709fa44510365413c7f46e74
Antaryami-044 <antaryamibabu1234@gmail.com>
2025-11-28 11:48:19 +0530

    best use of binary search problem is solved

--- DSA_c++/D14.cpp
+++ DSA_c++/D14.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 #include<math.h>
 
 using namespace std;
@@ -50,19 +51,31 @@ int floorCelliProblem(vector<int> &arr, int target) {
       return ans;
 }
 
+pair<int, int> findOccurrencesUsingBinarySearch(vector<int> &arr, int n, int target) {
+      int lb = lowerBound(arr, target);
+      int ub = upperBound(arr, target);
+      if(lb == n || arr[lb] != target) {
+            return {-1, -1};
+      }
+      return {lb, ub-1};
+}
+
 int main() {
       vector<int> arr = {1, 3, 3, 5, 7, 9};
       vector<int> arr2 = {1, 2, 4, 6, 8, 10};
       int target = 3;
       int target2 = 5;
+      int target3 = 3;
 
       int index = lowerBound(arr, target);
       int index2 = upperBound(arr, target);
       int floorValue = floorCelliProblem(arr2, target2);
+      pair<int, int> occurrences = findOccurrencesUsingBinarySearch(arr, arr.size(), target3);
 
       cout << "The lower bound index of " << target << " is: " << index << endl; 
       cout << "The upper bound index of " << target << " is: " << index2 << endl;   
       cout << "The floor value of " << target2 << " is: " << floorValue << endl;
+      cout << "The first and last occurrences of " << target3 << " are: (" << occurrences.first << ", " << occurrences.second << ")" << endl;
 
       return 0;
 }
